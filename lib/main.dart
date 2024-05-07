084b4a9ba91b159b68f8a80bb55cf4dfe26c59b7
Rizjky Dito Ridwansyah <rizjkyditoridwansyah@gmail.com>
2024-05-07 19:13:48 +0700

    File Aplikasi

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:aplikasi_flutter_pertama/row_widget.dart';
+import 'package:aplikasi_flutter_pertama/ui/produk_form.dart'; 
+import 'package:aplikasi_flutter_pertama/ui/produk_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Aplikasi Flutter Pertama",
+      home: ProdukPage()
+    );
+  }
+}
